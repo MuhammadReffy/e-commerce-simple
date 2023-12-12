595e25536c6049439ddb8737441cd5840ce2f426
Muhammad Reffy <syahnataa@gmail.com>
2023-12-12 22:03:53 +0700

    e-commerce sederhana

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter']
+      },
+      colors: {
+        primary: '#6643FF'
+      }
+    },
+  },
+  plugins: [],
+}
+
